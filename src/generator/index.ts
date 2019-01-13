a930cd25ab26a6f8d9e275036ac2993c3c2c8ada
Seung Ho <shjang1992@gmail.com>
2019-01-13 20:45:49 +0900

    DB connection fixed

--- src/generator/index.ts
+++ src/generator/index.ts
@@ -8,17 +8,21 @@ class Main {
     async run() {
         const dbUrl = process.env.DB_URL || "localhost";
         const dbPort = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306;
+        const dbUser = process.env.DB_USER || "root";
+        const dbPassword = process.env.DB_PASSWORD || "root";
         const dbDatabse = "tgd";
 
         const db = new DB({
             host: dbUrl,
             port: dbPort,
-            database: dbDatabse
+            database: dbDatabse,
+            user: dbUser,
+            password: dbPassword
         });
 
-        db.query("SELECT * FROM `articles` JOIN `streamer` ON `streamer`.`uid` = `articles`.`streamer_uid`");
+        const streamerName = "test1";
 
-        const articleFetcher = new ArticleFetcher("test1");
+        const articleFetcher = new ArticleFetcher(streamerName);
         const articles = await articleFetcher.getRecentArticles();
         console.log(articles);
     }
